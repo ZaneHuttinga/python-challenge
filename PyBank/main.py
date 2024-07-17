2e5f17f9a0d572d77385bd4053469a61f33e181a
Zane Huttinga <ZaneHuttinga@gmail.com>
2024-07-17 15:31:53 -0500

    Wrote PyBank script except printing to text file

---
+++ PyBank/main.py
@@ -0,0 +1,44 @@
+# Import modules
+import pandas as pd
+import csv
+
+# Load dataset from CSV file
+BudgetData = pd.read_csv("~/python-challenge/PyBank/Resources/budget_data.csv")
+
+
+
+# Define stats
+
+# Pull out second column
+ProfitLosses = BudgetData['Profit/Losses']
+
+# Basic stats
+months = len(ProfitLosses)
+net = sum(ProfitLosses)
+
+# Monthly changes
+changes = []
+dates = []
+for i in range (months-1):
+    dates.append(BudgetData['Date'][i+1])
+    j = ProfitLosses[i+1] - ProfitLosses[i]
+    changes.append(j)
+
+avg = sum(changes)/len(changes)
+increase = max(changes)
+decrease = min(changes)
+
+# Find dates for greatest increase and decrease
+inc_index = changes.index(increase)
+inc_date = dates[inc_index]
+dec_index = changes.index(decrease)
+dec_date = dates[dec_index]
+
+# Print analysis in terminal
+print("\nFinancial Analysis\n")
+print("----------------------------\n")
+print(f"Total Months: {months}\n")
+print(f"Total: ${net}\n")
+print(f"Average Change: ${avg:.2f}\n")
+print(f"Greatest Increase in Profits: {inc_date} ${increase}\n")
+print(f"Greatest Decrease in Profits: {dec_date} ${decrease}\n")
